e394e517057a567634fd4f81b0a373f8a52f39b7
Ashrafulhiderjayed <ashrafulhider87@gmail.com>
2024-11-04 17:01:24 +0600

    verify admin middleware

--- index.js
+++ index.js
@@ -44,21 +44,33 @@ async function run() {
       res.send({ token })
     })
 
-        //middlewares
-        const verifyToken = (req, res, next) => {
-          // console.log('inside verifyToken', req.headers);
-          if (!req.headers.authorization) {
-            return res.status(401).send({ message: 'unauthorized Access.' });
-          }
-          const token = req.headers.authorization.split(' ')[1];
-          jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
-            if (err) {
-              return res.status(403).send({ message: 'Invalid Token.' });
-            }
-            req.decoded = decoded;
-            next();
-          })
+    //middlewares
+    const verifyToken = (req, res, next) => {
+      // console.log('inside verifyToken', req.headers);
+      if (!req.headers.authorization) {
+        return res.status(401).send({ message: 'unauthorized Access.' });
+      }
+      const token = req.headers.authorization.split(' ')[1];
+      jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if (err) {
+          return res.status(403).send({ message: 'Invalid Token.' });
         }
+        req.decoded = decoded;
+        next();
+      })
+    }
+
+    //use verify admin after verifyToken
+    const verifyAdmin = async (req, res, next) => {
+      const email = req.decoded.email;
+      const query = { email: email };
+      const user = await userCollection.findOne(query);
+      const isAdmin = user?.role === "admin";
+      if (!isAdmin) {
+        return res.status(403).send({ message: 'forbidden access' });
+      }
+      next();
+    }
 
     //users related api ===============================
     app.post('/users', async (req, res) => {
